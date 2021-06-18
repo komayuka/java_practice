cf759b06e30dfebd7d3e3b6992b610c37e2b8d8f
komayuka <komayuka115@gmail.com>
2021-06-18 16:31:20 +0900

    java 8

---
+++ JavaWcp/src/Chapter07.java
@@ -0,0 +1,21 @@
+
+public class Chapter07 {
+	public static void main(String[] args) {
+		String color = "red";
+		switch(color){
+			case "red":
+				System.out.println("赤");
+				break;
+		    case "yellow":
+		    	System.out.println("黄");
+		    	break;
+		    case "blue":
+		    	System.out.println("青");
+		    	break;
+		    default:
+		    	System.out.println("信号ではない");
+		}
+	}
+
+}
+
