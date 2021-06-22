4f6895a6cfd9c5d9d2259f6707a15accd4b24a2e
komayuka <komayuka115@gmail.com>
2021-06-22 15:55:51 +0900

    [Chapter9]

--- JavaWcp/src/classMethod/human/Human05.java
+++ JavaWcp/src/classMethod/human/Human05.java
@@ -1,5 +1,20 @@
 package classMethod.human;
 
 public class Human05 {
+	private String name;
+	private int age;
+	
+	public Human05() {
+		name = "名前";
+		age = 20;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
 
 }
