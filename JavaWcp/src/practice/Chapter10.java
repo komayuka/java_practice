453e0727f3ae81ac4639f1848a9c6c1acd19e8bf
komayuka <komayuka115@gmail.com>
2021-06-28 11:24:15 +0900

    [Capster12]

--- JavaWcp/src/practice/Chapter10.java
+++ JavaWcp/src/practice/Chapter10.java
@@ -8,6 +8,7 @@ public class Chapter10 {
 			int array[] = { 1, 3, 5 };
 			Chapter10.validIndex(array, 2);
 			Chapter10.validIndex(array, 3);
+			
 			System.out.println("validIndexの呼び出し終了");
 		}catch (IllegalArgumentException e) {
 			System.out.println("IllegalArgumentExeptionが発生しました");
